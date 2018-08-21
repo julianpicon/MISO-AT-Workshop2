2d3be8669b377bce7d01a39ce3c2a11ef4c88801
Julian <julian.picon@hotmail.com>
2018-08-20 20:17:17 -0500

    Pruebas Protactor.

--- protactor/tour-of-heroes-cli/e2e/app.e2e-spec.ts
+++ protactor/tour-of-heroes-cli/e2e/app.e2e-spec.ts
@@ -16,6 +16,34 @@ describe('Tour of heroes Dashboard', () => {
     page.navigateToHeroes();
     expect(page.getAllHeroes().count()).toBe(11);
   });
+
+  it('should search hero', () => {
+    page.navigateTo();
+    const heroFound = page.heroNameToFind('Mr. Nice');
+    expect(heroFound.count()).toBe(1);
+  });
+
+  it('should navigate a heroe from Dashboard', () => {
+    page.navigateTo();
+    const heroFound = page.showHeroFromDashboard(1);
+    expect(heroFound.count()).toBe(1);
+    expect(heroFound.first().getText()).toBe('Narco details!');
+  });
+
+  it('should navigate to heroes from Heroes list', () => {
+    page.navigateToHeroes();
+    const heroFound = page.showHeroFromHeroesPage(1);
+    expect(heroFound.count()).toBe(1);
+    expect(heroFound.first().getText()).toBe('Mr. Nice details!');
+  });
+
+  it('should navigate to heroes from search control', () => {
+    page.navigateTo();
+    const heroFound = page.heroNameToFind('Bombasto');
+    expect(heroFound.count()).toBe(1);
+    expect(heroFound.first().getText()).toBe('Bombasto details!');
+  });
+
 });
 
 describe('Tour of heroes, heroes page', () => {
@@ -30,6 +58,21 @@ describe('Tour of heroes, heroes page', () => {
     const currentHeroes = page.getAllHeroes().count();
     page.enterNewHeroInInput('My new Hero');
     expect(page.getAllHeroes().count()).toBe(currentHeroes.then(n => n + 1));
+  });
+
+  it('should delete a hero', () => {
+    const currentHeroes = page.getAllHeroes().count();
+    page.deleteHero(1);
+    expect(page.getAllHeroes().count()).toBe(currentHeroes.then(n => n - 1));
+  });
+
+  it('should edit hero', () => {
+    page.navigateTo();
+    const heroFound = page.heroNameToFind('Tornado');
+    page.updateHeroName('Tornado v.2');
+    const heroFoundUpd = page.heroNameToFind('Tornado v.2');
+    expect(heroFound.count()).toBe(1);
+    expect(heroFound.first().getText()).toBe('Tornado v.2 details!');
   });
 
 });
--- protactor/tour-of-heroes-cli/e2e/app.po.ts
+++ protactor/tour-of-heroes-cli/e2e/app.po.ts
@@ -21,4 +21,32 @@ export class TourOfHeroesPage {
     element(by.tagName('input')).sendKeys(newHero);
     element(by.buttonText('Add')).click();
   }
+
+  heroNameToFind(hero: string) {
+    element(by.id('search-box')).sendKeys(hero);
+    element(by.css('.search-result')).click();
+    return element.all(by.tagName('h2'));
+  }
+
+  deleteHero(index: number) {
+    element(by.tagName('my-heroes')).all(by.css('.delete')).get(index).click()
+  }
+
+  updateHeroName(newHeroName: string) {
+    element(by.tagName('input')).clear()
+    element(by.tagName('input')).sendKeys(newHeroName);
+    element.all(by.tagName('button')).last().click();
+  }
+
+  showHeroFromDashboard(index: number){
+    element.all(by.css('.col-1-4')).get(index).click();
+    return element.all(by.tagName('h2'));
+  }
+
+  showHeroFromHeroesPage(index: number){
+    element(by.tagName('my-heroes')).all(by.tagName('li')).get(index).click();
+    element(by.buttonText('View Details')).click();
+    return element.all(by.tagName('h2'));
+  }
+
 }
